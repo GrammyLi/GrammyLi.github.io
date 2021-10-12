a04493e198f9f8e8ce36ff5f82bd006d4a776ca0
魔法师 <grammyli@outlook.com>
2021-10-12 09:46:41 +0800

    添加 github 头像生成页面
    页面基础架构完成

---
+++ avatar-generator/main.js
@@ -0,0 +1,14 @@
+/*
+ * @Author: your name
+ * @Date: 2021-10-12 09:44:43
+ * @LastEditTime: 2021-10-12 09:44:43
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: /grammyli/avatar-generator/main.js
+ */
+
+const main = () => {
+
+}
+
+main()
