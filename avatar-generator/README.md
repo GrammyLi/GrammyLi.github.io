109fa0153b6d872ae111830a6238cac365c11920
魔法师 <grammyli@outlook.com>
2021-10-12 10:04:32 +0800

    添加 计划

---
+++ avatar-generator/README.md
@@ -0,0 +1,10 @@
+<!--
+ * @Author: your name
+ * @Date: 2021-10-12 09:50:16
+ * @LastEditTime: 2021-10-12 09:50:16
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: /grammyli/avatar-generator/README.md
+-->
+## TODO
+
--- avatar-generator/main.js
+++ avatar-generator/main.js
@@ -1,14 +1,22 @@
 /*
  * @Author: your name
  * @Date: 2021-10-12 09:44:43
- * @LastEditTime: 2021-10-12 09:44:43
+ * @LastEditTime: 2021-10-12 09:50:26
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: /grammyli/avatar-generator/main.js
  */
-
-const main = () => {
-
+class Avatar {
+  static new(...args) {
+    return new this(...args);
+  }
+  constructor() {
+    // this.r = r;
+    // this.g = g;
+    // this.b = b;
+    // this.a = a;
+  }
 }
+const main = () => {};
 
-main()
+main();
