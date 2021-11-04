e1074a756c48c32f6140006bc79572e79518967a
魔法师 <grammyli@outlook.com>
2021-11-04 21:12:22 +0800

    页面基本成型 nb

---
+++ search/utils.js
@@ -0,0 +1,22 @@
+/*
+ * @Author: your name
+ * @Date: 2021-11-04 19:56:27
+ * @LastEditTime: 2021-11-04 19:56:27
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: /grammyli/search/utils.js
+ */
+const debug = true
+const log = debug ? console.log.bind(console) : function () { }
+const int = number => parseInt(number, 10)
+
+const bindEvent = (element, eventName, callback) => {
+  element.addEventListener(eventName, callback)
+}
+
+const appendHtml = (element, html) => {
+  element.insertAdjacentHTML('beforeend', html)
+}
+
+const e = selector => document.querySelector(selector)
+const es = selector => document.querySelectorAll(selector)
