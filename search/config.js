58414818e14fc71b56cda131bd6fdfa3b85979ad
魔法师 <grammyli@outlook.com>
2021-11-05 13:59:16 +0800

    完成 第一版工具

---
+++ search/config.js
@@ -0,0 +1,94 @@
+/*
+ * @Author: your name
+ * @Date: 2021-11-05 10:06:10
+ * @LastEditTime: 2021-11-05 13:51:51
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: /grammyli/search/config.js
+ */
+
+ /*
+ https://www.google.com.hk/search?q=keyword
+
+https://cn.bing.com/search?q=keyword&mkt=zh-CN
+
+https://www.zhihu.com/search?type=content&q=keyword
+
+https://search.bilibili.com/all?keyword=wefw&from_source=web_search
+
+https://www.douban.com/search?q=keyword
+
+https://weixin.sogou.com/weixinwap?type=2&query=keyword
+
+https://www.npmjs.com/search?q=keyword
+
+https://github.com/search?utf8=%E2%9C%93&q=keyword
+ */
+window.engines = [
+  {
+    id: 1,
+    url: "./img/icons8-google.svg",
+    text: "Google",
+    search: 'https://www.google.com.hk/search?q=keyword',
+  },
+  {
+    id: 2,
+    url: "./img/4375093_logo_overflow_stack_icon.svg",
+    text: "Stack overflow",
+    search: 'https://stackoverflow.com/nocaptcha?q=keyword',
+  },
+  {
+    id: 11,
+    url: "./img/icons8-qq.svg",
+    text: "Grammyli",
+    search: 'https://weixin.sogou.com/weixinwap?type=2&query=keyword',
+  },
+  {
+    id: 3,
+    url: "./img/icons8-bing.svg",
+    text: "Bing",
+    search: 'https://cn.bing.com/search?q=keyword&form=QBLH',
+  },
+  {
+    id: 4,
+    url: "./img/icons8-baidu.svg",
+    text: "Baidu",
+    search: 'https://cn.bing.com/search?q=keyword&mkt=zh-CN',
+  },
+  {
+    id: 5,
+    url: "./img/icons8-github.svg",
+    text: "Github",
+    search: 'https://github.com/search?utf8=%E2%9C%93&q=keyword',
+  },
+  {
+    id: 6,
+    url: "./img/juejin.svg",
+    text: "Juejin",
+    search: 'https://juejin.cn/search?query=keyword',
+  },
+  {
+    id: 7,
+    url: "./img/douban.svg",
+    text: "Douban",
+    search: 'https://www.douban.com/search?q=keyword',
+  },
+  {
+    id: 8,
+    url: "./img/7417375_bilibili_b site_logo_video_website_icon.svg",
+    text: "Bilibili",
+    search: 'https://search.bilibili.com/all?keyword=wefw&from_source=web_search',
+  },
+  // {
+  //   id: 9,
+  //   url: "./img/214732_zhihu_china_chinese_icon.svg",
+  //   text: "Zhihu",
+  //   search: 'https://www.zhihu.com/search?type=content&q=keyword',
+  // },
+  {
+    id: 10,
+    url: "./img/Mdn.svg",
+    text: "MDN",
+    search: 'https://developer.mozilla.org/zh-CN/search?q=keyword',
+  },
+];
--- search/main.js
+++ search/main.js
@@ -1,10 +1,124 @@
 /*
  * @Author: your name
  * @Date: 2021-10-14 13:35:59
- * @LastEditTime: 2021-11-04 12:40:58
+ * @LastEditTime: 2021-11-05 13:55:08
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: /grammyli/search/main.js
  */
- 
-  
+const templateEngine = ({ id, url, text, search }, index) => {
+  return `
+  <div class="g-engine g-engine-${id} g-drag" data-index="${index}">
+    <img src="${url}"  data-action="clickEngineCard"  />
+    <span>${text}</span>
+  </div>
+  `;
+};
+
+const templateLogoEngine = ({ id, url, text, search }) => {
+  return `
+  <div class="g-engine">
+    <img src="${url}"  />
+    <span>${text}</span>
+  </div>
+  `;
+};
+
+const renderEngines = () => {
+  let t = window.engines.map((e, i) => templateEngine(e, i)).join("");
+  appendHtml(e(".g-engines"), t);
+};
+
+const renderLogo = () => {
+  let engine = window.engines[currentEngineIndex];
+  let t = templateLogoEngine(engine);
+  const center = e(".g-center");
+  // 先清空画布
+  center.innerHTML = "";
+  center.innerHTML = t;
+  hideLogoBtn()
+};
+
+const init = () => {
+  renderEngines();
+  renderLogo();
+};
+
+const bindEventKeyup = () => {
+  bindEvent(window, "keyup", (event) => {
+    if (event.key === "Enter") {
+      actions.searchContent();
+    }
+  });
+};
+
+const hideLogoBtn = () => {
+  if (currentEngineIndex === 0) {
+    e(".g-left-img").classList.add('g-hide');
+    e(".g-right-img").classList.remove('g-hide');
+  } else if (currentEngineIndex === engines.length - 1) {
+    e(".g-right-img").classList.add('g-hide');
+    e(".g-left-img").classList.remove('g-hide');
+  } else {
+    e(".g-right-img").classList.remove('g-hide');
+    e(".g-left-img").classList.remove('g-hide');
+  }
+};
+
+const actions = {
+  clearInputValue() {
+    e(".g-input__search").value = "";
+  },
+  clickEngineCard(event) {
+    // 更新搜索方式
+    let engine = event.target.closest(".g-engine");
+    window.currentEngineIndex = Number(engine.dataset.index);
+    renderLogo();
+  },
+  clickControlBtn(event) {
+    // 更新搜索方式
+    let btn = event.target.closest(".btn");
+    let offset = Number(btn.dataset.offset);
+    currentEngineIndex += offset;
+    if (currentEngineIndex < 0) {
+      currentEngineIndex = 0;
+    }
+    if (currentEngineIndex >= engines.length) {
+      currentEngineIndex = engines.length - 1;
+    }
+    log("currentEngineIndex", currentEngineIndex);
+    renderLogo();
+  },
+  searchContent() {
+    let v = e(".g-input__search").value;
+    log("开始搜索", v);
+    if (v.length === 0) {
+      return;
+    }
+    let link = e(".g-search-link");
+    let engine = window.engines[currentEngineIndex];
+    link.href = engine.search.replace("keyword", v);
+    link.click();
+  },
+};
+
+const bindEventClick = () => {
+  bindEvent(e("body"), "click", (event) => {
+    let action = event.target.dataset.action;
+    log("action", action);
+    actions[action] && actions[action](event);
+  });
+};
+
+const bindEvents = () => {
+  bindEventKeyup();
+  bindEventClick();
+};
+
+const __main = () => {
+  window.currentEngineIndex = 2;
+  init();
+  bindEvents();
+};
+
+__main();
