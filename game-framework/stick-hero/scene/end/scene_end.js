b26bf50af88525b7513c30d07959e60410b4198c
魔法师 <grammyli@outlook.com>
2021-11-12 20:12:04 +0800

    add: stick hero

---
+++ game-framework/stick-hero/scene/end/scene_end.js
@@ -0,0 +1,14 @@
+/*
+ * @Author: your name
+ * @Date: 2020-03-29 21:30:40
+ * @LastEditTime: 2021-11-12 20:09:03
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: /grammyli/game-framework/bird/scene/end/scene_end.js
+ */
+class SceneEnd extends Scene {
+    constructor(game) {
+        super(game)
+         
+    }
+}
---
+++ game-framework/stick-hero/scene/title/scene_title.js
@@ -0,0 +1,15 @@
+/*
+ * @Author: your name
+ * @Date: 2020-05-31 11:19:00
+ * @LastEditTime: 2021-11-12 20:06:15
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: /grammyli/game-framework/bird/scene/title/scene_title.js
+ */
+class SceneTitle extends Scene {
+    constructor(game) {
+        super(game)
+        
+    }
+   
+}
