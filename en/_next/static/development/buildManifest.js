c45441c681b085b0bfdc4d78087e9f6be427bd03
魔法师 <grammyli@outlook.com>
2021-11-19 09:22:36 +0800

    Revert "update en"
    This reverts commit 4c17cda0c7eaa74527d9d2d5aa12aea44222be9b.

--- en/_next/static/development/buildManifest.js
+++ en/_next/static/development/buildManifest.js
@@ -1,9 +1,1 @@
-/*
- * @Author: your name
- * @Date: 2021-11-18 21:14:14
- * @LastEditTime: 2021-11-19 09:19:47
- * @LastEditors: your name
- * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
- * @FilePath: /grammyli/en/_next/static/development/buildManifest.js
- */
-self.__BUILD_MANIFEST = {__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static\u002Fchunks\u002Fpages\u002Findex.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error.js"],sortedPages:["\u002F","\u002Fapp","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
+self.__BUILD_MANIFEST = {__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static\u002Fchunks\u002Fpages\u002Findex.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
