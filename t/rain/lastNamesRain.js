81d144541bb2e8a24bb6d29d63fe04e111a32cc5
魔法师 <grammyli@outlook.com>
2022-02-13 13:13:13 +0800

    refactor: rain

--- t/rain/lastNamesRain.js
+++ t/rain/lastNamesRain.js
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2022-02-13 13:02:02
- * @LastEditTime: 2022-02-13 13:02:03
+ * @LastEditTime: 2022-02-13 13:10:45
  * @LastEditors: Please set LastEditors
  * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  * @FilePath: /rain/lastNamesRain.js
@@ -11,26 +11,32 @@ class LastNamesRain extends G {
     super()
     this.canvas = canvas;
     this.fontSize = fontSize;
-    this.ctx = canvas.getContext("2d");
+    this.context = canvas.getContext("2d");
     this.setup();
     this.drawRain();
   }
   setup() {
-    const config = {
-      canvas: this.canvas,
-      fontSize: this.fontSize,
-    };
-    this.rain = Rain.new(config);
+    this.columns = this.canvas.width / this.fontSize;
+    this.raindrops = [];
+    for (let i = 0; i < this.columns; i++) {
+      this.raindrops[i] = Raindrop.new(
+        i,
+        0,
+        this.fontSize,
+        this.canvas.height,
+        this.context
+      );
+    }
   }
   cleanRain() {
-    const { ctx, canvas } = this;
-    ctx.fillStyle = "rgba(0, 0, 0, 0.05)";
-    ctx.fillRect(0, 0, canvas.width, canvas.height);
-    ctx.font = this.fontSize + "px monospace";
+    const { context, canvas } = this;
+    context.fillStyle = "rgba(0, 0, 0, 0.05)";
+    context.fillRect(0, 0, canvas.width, canvas.height);
+    context.font = this.fontSize + "px monospace";
   }
   drawRain() {
     this.cleanRain();
-    this.rain.raindrops.forEach((s) => s.draw());
+    this.raindrops.forEach((s) => s.draw());
     setTimeout(() => {
       this.drawRain();
     }, 10);
