fda39129cf5c4b90d9e9ededc1334d2ed6338ec7
魔法师 <grammyli@outlook.com>
2022-03-04 19:08:52 +0800

    添加橡皮功能

--- t/paint/paint.js
+++ t/paint/paint.js
@@ -1,21 +1,33 @@
 class Paint extends G {
   constructor({ selector, w, h }) {
     super();
-    this.canvas = e(selector)
+    this.canvas = e(selector);
     this.canvas.width = w;
     this.canvas.height = h;
     this.ctx = this.canvas.getContext("2d");
-    this.ctx.strokeStyle = "#2c2c2c";
-    this.ctx.lineWidth = 2.5;
-    this.painting = false;
+    this.type = "painting";
+    this.typeStatus = {
+      erasering: false,
+      painting: false,
+    };
     this.bindEvents();
   }
+  updatePaintConfig() {
+    if (this.typeStatus.erasering) {
+      this.ctx.strokeStyle = "white";
+      this.ctx.lineWidth = 4;
+    } else {
+      this.ctx.strokeStyle = "#2c2c2c";
+      this.ctx.lineWidth = 2.5;
+    }
+  }
   bindEventMove() {
-    const { canvas, ctx,} = this;
+    const { canvas, ctx } = this;
     bindEvent(canvas, "mousemove", (event) => {
+      this.updatePaintConfig()
       const x = event.offsetX;
       const y = event.offsetY;
-      if (!this.painting) {
+      if (!this.typeStatus[this.type]) {
         ctx.beginPath();
         ctx.moveTo(x, y);
       } else {
@@ -27,19 +39,25 @@ class Paint extends G {
   bindEventDown() {
     const { canvas } = this;
     bindEvent(canvas, "mousedown", (event) => {
-      this.painting = true;
+      this.typeStatus[this.type] = true;
     });
   }
   bindEventUp() {
     const { canvas } = this;
     bindEvent(canvas, "mouseup", (event) => {
-      this.painting = false;
+      this.typeStatus[this.type] = false;
     });
   }
   bindEventLeave() {
     const { canvas } = this;
     bindEvent(canvas, "mouseleave", (event) => {
-      this.painting = false;
+      this.typeStatus[this.type] = false;
+    });
+  }
+  bindEvnetType() {
+    bindEvent(e(".controls__type"), "click", (event) => {
+      const target = event.target;
+      this.type = target.dataset.type
     });
   }
   bindEvents() {
@@ -47,5 +65,6 @@ class Paint extends G {
     this.bindEventDown();
     this.bindEventUp();
     this.bindEventLeave();
+    this.bindEvnetType();
   }
 }
