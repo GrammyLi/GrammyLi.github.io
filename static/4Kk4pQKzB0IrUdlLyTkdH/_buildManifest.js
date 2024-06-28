5b6783c79f8e2e1f4407d859b426f6e2137b17f2
lichao <lichao@yilan.tv>
2024-06-28 17:26:22 +0800

    fix:

--- static/4Kk4pQKzB0IrUdlLyTkdH/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fchunks\u002Fpages\u002Findex-fe94302870e331ce972e.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-5c87f7ea3f59b2331d4c.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
---
+++ static/P7ufNg0nB3EfDHg8YGHAG/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fchunks\u002Fpages\u002Findex-31c4e413ca703155d4bb.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-5c87f7ea3f59b2331d4c.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
